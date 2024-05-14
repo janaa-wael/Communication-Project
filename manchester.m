eecde976f4f2792840cef7aeb68ec8a787838010
sh3141 <158093242+sh3141@users.noreply.github.com>
2024-05-14 22:46:35 +0300

    added v_high argument

--- manchester.m
+++ manchester.m
@@ -1,4 +1,4 @@
-function [encoded_signal]=manchester(stream,n)
+function [encoded_signal]=manchester(stream,n,v_high)
   %manchester line coding
   %@ stream : row vector containing the stream of bits
   %@ n : half the number of points in line coding that represent a single bit
@@ -7,10 +7,10 @@ function [encoded_signal]=manchester(stream,n)
         % If the current bit is 0
         if stream(i) == 0
             % Append a low-to-high transition followed by a high-to-low transition
-        encoded_signal = [encoded_signal, repmat([-1,-1], 1, n), repmat([1,1], 1, n)];
+        encoded_signal = [encoded_signal, repmat([-1*v_high,-1*v_high], 1, n), repmat([v_high,v_high], 1, n)];
         % If the current bit is 1
         elseif stream(i) == 1
             % Append a high-to-low transition followed by a low-to-high transition
-        encoded_signal = [encoded_signal, repmat([1,1], 1, n), repmat([-1,-1], 1, n)];
+        encoded_signal = [encoded_signal, repmat([v_high,v_high], 1, n), repmat([-1*v_high,-1*v_high], 1, n)];
         end
     end
