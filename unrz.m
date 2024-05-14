3a55bf0f353a57ee00c85d10da7894242cc38890
sh3141 <158093242+sh3141@users.noreply.github.com>
2024-05-14 22:47:53 +0300

    added v_high argument

--- unrz.m
+++ unrz.m
@@ -1,4 +1,4 @@
-function [signal]=unrz(stream,t)
+function [signal]=unrz(stream,t,v_high)
 %unipolar non return to zero
 %@ stream: stream of bits
 %@ t: time vector of the linecoding
@@ -6,8 +6,7 @@ signal = zeros(1, length(t));
 for j = 1:length(stream)
         % If the current bit is 1, set the corresponding time duration to high
         if stream(j) == 1
-            signal((j-1)*length(t)/length(stream) + 1 : j*length(t)/length(stream)) = 1;
+            signal((j-1)*length(t)/length(stream) + 1 : j*length(t)/length(stream)) = v_high;
         end
     end
-
 
