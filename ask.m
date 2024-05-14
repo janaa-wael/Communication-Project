296c496590c8bbd7a230d4364f7b082980b4f75f
sh3141 <158093242+sh3141@users.noreply.github.com>
2024-05-15 00:06:59 +0300

    Update ask.m

--- ask.m
+++ ask.m
@@ -1,13 +1,16 @@
 bit_no =64;
+v_high=1.2;
 stream = randi([0, 1], 1, bit_no);
-bitrate=1;
-fc = 3 ;
-duration = 64;
-Fs = 100; %sampling frequence
-ts = 1/Fs ;
-%signal after applying linecoding:
-t = linspace(0, duration, duration * Fs);     %time vector
-signal = unrz(stream,t);
+bitrate=100;
+bit_dur=1/bitrate;
+Fs=5*bitrate; %sampling frequency
+ts=1/Fs;
+
+fc = 10*bitrate ; %carrier frequency
+
+%signal after applying unipolar non return to zero linecoding:
+t = 0:ts:bit_no*(bit_dur)-ts;     %time vector
+signal = unrz(stream,t,v_high);
 
 %%%%%%%%%%%%%% modulation %%%%%%%%%%%%%%%%%%%%%%%%
 carrier_signal=cos(2*pi*fc*t);
@@ -51,18 +54,28 @@ received_signal = ASK_signal + noise;
 phases =[30,60,90];
 
 for j=1:length(phases)
-  local_oscillator=sin(2*pi*t*fc+deg2rad(phases(j)));
+  local_oscillator=cos(2*pi*t*fc+deg2rad(phases(j)));
   demodulated_signal= received_signal.*local_oscillator;
+%low pass filter
+
+%parameters:
+f_cutoff = 10+bitrate;%cutoff frequency
+order=5; %order of butterworth filter
+
+[a,b]=butter(order,f_cutoff/(Fs/2),"low");
+
+output_signal=filter(a,b,demodulated_signal);
 
   %quantising recieved signal :
 
-Vth = 0.5;
-region = length(t) /Fs;
+Vth = v_high/2;
+region = bit_no;
+dur_per_bit=length(t)/bit_no;
 receiver_output = zeros(1, region);
 for i = 1:region
-    regionStart = (i - 1) * 100 + 1;
-    regionEnd = i * 100;
-    region = demodulated_signal(regionStart:regionEnd);
+    regionStart = (i - 1) * dur_per_bit + 1;
+    regionEnd = i * dur_per_bit;
+    region = output_signal(regionStart:regionEnd);
     if any(region > Vth)
         receiver_output(regionStart:regionEnd) = 1;
     else
@@ -79,13 +92,14 @@ endfor
     %ylim([-0.5, 1.5]);
     grid on;
 
-  %spectral plots of ask modulated signal:
+
+  %spectral plots of ask demodulated signal:
     figure;
     L = length(receiver_output);
     Y = fftshift(fft(receiver_output))*1/Fs;
     N = L;
     df = Fs/N;
-    if(rem(N,2)==0) %% Even
+     if(rem(N,2)==0) %% Even
       f = - (0.5*Fs) : df : (0.5*Fs-df) ; %% Frequency vector if x/f is even
     else %% Odd
       f = - (0.5*Fs-0.5*df) : df : (0.5*Fs-0.5*df) ; %% Frequency vector if x/f is odd
@@ -94,6 +108,4 @@ endfor
        title(sprintf('Frequency Spectrum of ASK demodulated stream with oscillator phase %d°',phases(j)));
       xlabel('Frequency (Hz)');
       ylabel('Amplitude');
-
-
 end
